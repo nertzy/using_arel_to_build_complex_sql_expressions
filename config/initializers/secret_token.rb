a696c61916ba68871601e707e82ec33d7705a5e5
Grant Hutchins <nertzy@gmail.com>
2013-02-08 13:24:18 -0500

    Basic Rails app with Blog Post model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trigram::Application.config.secret_token = '9b45ac590575db63ddafe46460744ba327d63086b821b761ba4c0616d2b36b446a00660f0aae56c9b2e1c93e4f0d75524f0481f02f11d0ce9badd31acfee357f'
